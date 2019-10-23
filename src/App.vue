7e7f73b641ab4c210e369d4f7c6c09bd099d2ec9
Michael Alexander Robertson <MJCRobertson@outlook.com>
2019-10-23 14:50:09 +0100

    mvp complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div>
+  <h1>Countries</h1>
+  <div class="main-container">
+
+    <countries-list :countries="countries"></countries-list>
+    <country-detail :country="selectedCountry"></country-detail>
+
   </div>
+
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from "./components/CountriesList.vue";
+import CountryDetail from "./components/CountryDetail.vue";
+import { eventBus } from "./main.js";
 
 export default {
   name: 'app',
+  data(){
+    return {
+    countries: [],
+    selectedCountry: null
+    };
+  },
+  mounted(){
+    eventBus.$on("country-selected", (country) => {
+      this.selectedCountry = country;
+    });
+
+
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(data => data.json())
+    .then(countries => this.countries = countries)
+  },
   components: {
-    HelloWorld
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
